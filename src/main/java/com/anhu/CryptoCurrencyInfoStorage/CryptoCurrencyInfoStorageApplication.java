17413accb97356b3ce6ae54d5d6b04e5a677b9e0
Anneke <huijsmans90@gmail.com>
2021-06-23 21:18:47 +0200

    Added spring Initializr files

---
+++ src/main/java/com/anhu/CryptoCurrencyInfoStorage/CryptoCurrencyInfoStorageApplication.java
@@ -0,0 +1,13 @@
+package com.anhu.CryptoCurrencyInfoStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyInfoStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCurrencyInfoStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhu/CryptoCurrencyInfoStorage/CryptoCurrencyInfoStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhu.CryptoCurrencyInfoStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyInfoStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
